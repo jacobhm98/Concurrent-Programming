f91834b1f1f482d1bcb01e3df265f4d8e7f177f8
jacobhm98 <jacmalm@kth.se>
2020-02-05 16:25:49 +0100

    stored all the words in a set we can do .contains() operations on, and started on a isPalindrome method

--- hw2/palindrome.cpp
+++ hw2/palindrome.cpp
@@ -6,6 +6,7 @@
 #include <fstream>
 #include <iostream>
 #include <string>
+#include <set>
 using namespace std;
 
 int main (){
@@ -13,15 +14,25 @@ int main (){
 	ofstream out;
 	in.open("dictionary/words");
 	out.open("dictionary/palindromes");
+	set<string> wordList;
 	if (!in) {
 		cerr << "there is no dictionary file at ./dictionary/words" << endl;
 		return 1;
 	}
 	string word;
 	while (in >> word){
-		out << word << "\n";
+		wordList.insert(word);	
 	}
+	cout << "The size of the wordList is: " << wordList.size() << endl;
 	cout << "palindromes printed to ./dictionary/palindromes" << endl;
+	return 0;
+}
 
-
+bool isPalindrome(string word){
+	bool isPalindrome = false;
+	if (word.length() == 1){
+		isPalindrome = true;
+		return isPalindrome;
+	}
+	return isPalindrome;
 }

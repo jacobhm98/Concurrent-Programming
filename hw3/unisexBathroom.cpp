77d708cc597e3425e422aa16672ba06ce28a2dd3
jacobhm98 <jacmalm@kth.se>
2020-02-14 15:43:33 +0100

    rebuilt skeleton a bit

--- hw3/unisexBathroom.cpp
+++ hw3/unisexBathroom.cpp
@@ -17,14 +17,16 @@ using std::cout;
 using std::endl;
 
 //function declarations
-void * Work(void *);
+void * mWork(void *);
+void * wWork(void *);
 
 //global variables n synchronization structures
 sem_t lock;
 sem_t mEnter;
 sem_t wEnter;
 int bathroomUsers = 0;
-
+int menWaiting = 0;
+int womenWaiting = 0;
 
 int main(int argc, char **argv){
 	sem_init(&lock, SHARED, 1);
@@ -36,16 +38,14 @@ int main(int argc, char **argv){
 	int numWomen;
 	numMen = (argc > 1) ? atoi(argv[1]) : DEFAULT_SIZE;
 	numWomen = (argc > 2) ? atoi(argv[2]) : DEFAULT_SIZE;
-	long genderM = 0;
-	long genderW = 1;
 	pthread_t men[numMen];
 	pthread_t women[numWomen];
 
-	for (int i = 0; i < numMen; i++){
-		pthread_create(&men[i], NULL, Work, (void *) genderM);
+	for (long i = 0; i < numMen; i++){
+		pthread_create(&men[i], NULL, mWork, (void *) i);
 	}
-	for (int i = 0; i < numWomen; i++){
-		pthread_create(&women[i], NULL, Work, (void *) genderW);
+	for (long i = 0; i < numWomen; i++){
+		pthread_create(&women[i], NULL, wWork, (void *) (i + numMen));
 	}
 	for (int i = 0; i < numMen; i++){
 		pthread_join(men[i], NULL);
@@ -57,38 +57,33 @@ int main(int argc, char **argv){
 
 }
 
-void * Work(void * gender){
-	long myGender = (long) gender;
+void * mWork(void * id){
+	long myId = (long) id;
 	sem_wait(&lock);
 	unsigned int workTime = rand() % 60;
 	cout << "thread " << pthread_self() << " is boutta work for: " << workTime << " seconds" << endl;
 	sem_post(&lock);
 	sleep(workTime);
 	cout << "thread " << pthread_self() << " needs to go to the bathroom!" << endl;
-	if (gender == 0){
-		sem_wait(&mEnter);
 
-	}
-	if (gender == 1){
-		sem_wait(&wEnter);
-	}
 	return NULL;
 }
 
-void useBathroom(long gender){a
-	if (gender == 0){
-		if (bathroomUsers == 0){
-			sem_wait(&wEnter);
-		}
-		sem_post(&mEnter);
-	}
-	else {
-		if (bathroomUsers == 0){
-			sem_wait(&mEnter);
-		}
-		sem_post(&wEnter);
-	}
-	bathroomUsers++;
-
-
+void * wWork(void * id){
+	long myId = (long) id;
+	sem_wait(&lock);
+	unsigned int workTime = rand() % 60;
+	cout << "thread " << pthread_self() << " is boutta work for: " << workTime << " seconds" << endl;
+	sem_post(&lock);
+	sleep(workTime);
+	cout << "thread " << pthread_self() << " needs to go to the bathroom!" << endl;
+	return NULL;
+}
+void useBathroom(long id){
+	sem_wait(&lock);
+	unsigned int bathroomTime = rand() % 5;
+	cout << "Worker number " << id << "is using the bathroom for " << bathroomTime << "seconds!" << endl;
+	sem_post(&lock);
+	sleep(bathroomTime);
+	return;
 }

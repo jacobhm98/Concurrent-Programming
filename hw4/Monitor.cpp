406a0e22f721f7f97d0f9da9f020086e70c165c5
jacobhm98 <jacmalm@kth.se>
2020-02-19 17:19:31 +0100

    skeleton of monitor done

--- hw4/Monitor.cpp
+++ hw4/Monitor.cpp
@@ -1,6 +1,21 @@
+
+#ifndef _REENTRANT
+#define _REENTRANT
+#endif
 #include "Monitor.h"
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <unistd.h>
 
+Monitor::Monitor(){
 
+	pthread_mutex_init(&lock, NULL);
+	pthread_cond_init(&menQueue, NULL);
+	pthread_cond_init(&womenQueue, NULL);
+	menUsingBathroom = 0;
+	womenUsingBathroom = 0;
+}
 
 void Monitor::manEnter(){
 	
--- hw4/Monitor.h
+++ hw4/Monitor.h
@@ -4,10 +4,16 @@
 
 #ifndef MONITOR_H
 #define MONITOR_H
+#include <pthread.h>
 
 class Monitor{
-
+	int womenUsingBathroom;
+	int menUsingBathroom;
+	pthread_mutex_t lock;
+	pthread_cond_t menQueue;
+	pthread_cond_t womenQueue;	
 	public:
+		Monitor();
 		void manEnter();
 		void womanEnter();
 		void manExit();
